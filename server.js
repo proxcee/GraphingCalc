82e00839469eb4e93cf50177f17dd743c87e87b9
proxcee <tcarns2010@gmail.com>
2025-03-03 14:02:20 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const app = express();
+
+app.use('/', createProxyMiddleware({
+  target: 'https://www.google.com', // Change this if you want to proxy a different site
+  changeOrigin: true,
+  secure: false
+}));
+
+app.listen(3000, () => {
+  console.log('Proxy running on port 3000');
+});
 
