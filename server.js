dccd958c96abb38f1b1638c85c76e05c1b90e330
proxcee <tcarns2010@gmail.com>
2025-03-03 14:13:55 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
-const express = require('express');
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const express = require("express");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
 const app = express();
 
-app.use('/', createProxyMiddleware({
-  target: 'https://www.google.com', // Change this if you want to proxy a different site
+app.use("/", createProxyMiddleware({
+  target: "https://www.google.com", // Change this to the default website you want to proxy
   changeOrigin: true,
-  secure: false
+  pathRewrite: function (path, req) {
+    return "/";
+  },
+  onProxyReq: (proxyReq, req, res) => {
+    proxyReq.setHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64)");
+  }
 }));
 
-app.listen(3000, () => {
-  console.log('Proxy running on port 3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Proxy running on port ${PORT}`);
 });
-
